996ed219e3aa4ddf5b2830d5243d5c2fbe16ac8e
Ibirn <kmcricciuti@gmail.com>
2020-08-26 20:54:21 +0000

    multiple snake bug fixed. responsive wasd control and two canned sayings.

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -8,12 +11,24 @@ const setupInput = () => {
   const handleUserInput = (key) => {
     if (key === '\u0003') {
       process.exit();
-  }}
+    } else if (key === 'w'){
+      conn.write("Move: up")
+    } else if (key === 's'){
+      conn.write("Move: down")
+    } else if (key === 'a'){
+      conn.write("Move: left")
+    } else if (key === 'd'){
+      conn.write("Move: right")
+    } else if (key === 'q'){
+      conn.write("Say: We are snake legion")
+    } else if (key === 'e'){
+      conn.write("Say: SSSSSSSnek")
+    }
+  }
   //stdin listening for data (input) and when detected, runs handleUserInput with that keypress as argument.
   stdin.on('data', (key) => {
     handleUserInput(key)
   })
-
   return stdin;
 }
 
--- play.js
+++ play.js
@@ -3,6 +3,6 @@ const { setupInput } = require("./input")
 //removed connect function to ./client.js
 //removed input function to ./input.js
 console.log('Connecting ...');
-connect();
+//connect();
 
-setupInput();
+setupInput(connect());
