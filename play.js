51a73c4a6fcde445967f7da21a97d55ae0eb51a4
Ibirn <kmcricciuti@gmail.com>
2020-08-26 20:21:04 +0000

    added quit funciton to snake game.

--- play.js
+++ play.js
@@ -2,3 +2,26 @@ const { connect } = require("./client.js")
 //removed connect function to ./client.js
 console.log('Connecting ...');
 connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  //escape function that takes argument and exits:
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+  }}
+  //stdin listening for data (input) and when detected, runs handleUserInput with that keypress as argument.
+  stdin.on('data', (key) => {
+    handleUserInput(key)
+  })
+
+  return stdin;
+}
+
+
+
+setupInput();
