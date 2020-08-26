cfb4a4385852d6c1df9500b0d4d54dffb4c39ada
Ibirn <kmcricciuti@gmail.com>
2020-08-26 20:24:44 +0000

    moved input function to it's own js module.

---
+++ input.js
@@ -0,0 +1,22 @@
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  //escape function that takes argument and exits:
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+  }}
+  //stdin listening for data (input) and when detected, runs handleUserInput with that keypress as argument.
+  stdin.on('data', (key) => {
+    handleUserInput(key)
+  })
+
+  return stdin;
+}
+
+module.exports = {
+  setupInput,
+}
--- play.js
+++ play.js
@@ -1,27 +1,8 @@
 const { connect } = require("./client.js")
+const { setupInput } = require("./input")
 //removed connect function to ./client.js
+//removed input function to ./input.js
 console.log('Connecting ...');
 connect();
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  //escape function that takes argument and exits:
-  const handleUserInput = (key) => {
-    if (key === '\u0003') {
-      process.exit();
-  }}
-  //stdin listening for data (input) and when detected, runs handleUserInput with that keypress as argument.
-  stdin.on('data', (key) => {
-    handleUserInput(key)
-  })
-
-  return stdin;
-}
-
-
 
 setupInput();
