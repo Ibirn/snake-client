1a83ebec774972a6a70f94fb65a189b4c1b6b488
Ibirn <kmcricciuti@gmail.com>
2020-08-26 19:52:35 +0000

    added ability to read server messages.

--- play.js
+++ play.js
@@ -1,11 +1,15 @@
 const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
+  //below added to read messages from server:
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
 
   return conn;
 }
